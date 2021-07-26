8b98e6039649ad423c8f4ab06303741c19d303b5
Koo Zheng Yu <zykoo@palo-it.com>
2021-07-26 17:28:58 +0800

    add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
+import 'react-native-gesture-handler';
+
 import { Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>Hello, world!</Text>
-      </View>
+      <NavigationContainer>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Hello, world!</Text>
+        </View>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
