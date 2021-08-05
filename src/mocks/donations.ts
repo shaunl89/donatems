c9bdb9c0ec253b16703bdaaad2017c91a9bcdfa7
Shaun Loh <shaun_loh@tech.gov.sg>
2021-08-05 12:11:42 +0800

    added new images

--- src/mocks/donations.ts
+++ src/mocks/donations.ts
@@ -6,11 +6,17 @@ const IMG_TOP_1 = require("../images/Baby_Tops_1.jpg");
 const IMG_TOP_2 = require("../images/Baby_Tops_2.jpg");
 const IMG_TOP_3 = require("../images/Baby_Tops_3.jpg");
 const IMG_TOP_4 = require("../images/Baby_Tops_4.jpg");
+const IMG_TOP_5 = require("../images/Tops_5.jpg");
+const IMG_TOP_6 = require("../images/Tops_6.jpeg");
+const IMG_TOP_7 = require("../images/Tops_7.jpeg");
+
 const IMG_BTM_1 = require("../images/Baby_Bottoms_1.jpg");
 const IMG_BTM_2 = require("../images/Baby_Bottoms_2.jpg");
+const IMG_BTM_3 = require("../images/Bottoms_4.jpeg");
+
 const IMG_FTW_1 = require("../images/Baby_Footwear_1.png");
-
-
+const IMG_FTW_2 = require("../images/Footwear_1.jpeg");
+const IMG_FTW_3 = require("../images/Footwear_2.jpeg");
 
 export const allDonations: Donation[] = [
   {
@@ -23,7 +29,16 @@ export const allDonations: Donation[] = [
     status: Status.AVAILABLE,
   },
   {
-    uri: IMG_TOP_2,
+    uri: IMG_TOP_6,
+    category: Category.TOPS,
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, quam, recusandae quidem sed consectetur nulla voluptate repudiandae quod aperiam veritatis voluptatibus maiores nostrum id nam est. Quibusdam provident animi numquam.',
+    giver: allUsers[1],
+    receiver: null,
+    status: Status.AVAILABLE,
+  },
+  {
+    uri: IMG_TOP_5,
     category: Category.TOPS,
     description:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, quam, recusandae quidem sed consectetur nulla voluptate repudiandae quod aperiam veritatis voluptatibus maiores nostrum id nam est. Quibusdam provident animi numquam.',
@@ -41,7 +56,7 @@ export const allDonations: Donation[] = [
     status: Status.AVAILABLE,
   },
   {
-    uri: IMG_FTW_1,
+    uri: IMG_FTW_2,
     category: Category.FOOTWEAR,
     description:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, quam, recusandae quidem sed consectetur nulla voluptate repudiandae quod aperiam veritatis voluptatibus maiores nostrum id nam est. Quibusdam provident animi numquam.',
@@ -50,7 +65,7 @@ export const allDonations: Donation[] = [
     status: Status.AVAILABLE,
   },
   {
-    uri: IMG_FTW_1,
+    uri: IMG_FTW_3,
     category: Category.FOOTWEAR,
     description:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, quam, recusandae quidem sed consectetur nulla voluptate repudiandae quod aperiam veritatis voluptatibus maiores nostrum id nam est. Quibusdam provident animi numquam.',
@@ -80,7 +95,16 @@ export const mySelection: Donation[] = [
     status: Status.CHOPED,
   },
   {
-    uri: IMG_BTM_2,
+    uri: IMG_TOP_7,
+    category: Category.TOPS,
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, quam, recusandae quidem sed consectetur nulla voluptate repudiandae quod aperiam veritatis voluptatibus maiores nostrum id nam est. Quibusdam provident animi numquam.',
+    giver: allUsers[1],
+    receiver: allUsers[2],
+    status: Status.CHOPED,
+  },
+  {
+    uri: IMG_BTM_3,
     category: Category.BOTTOMS,
     description:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, quam, recusandae quidem sed consectetur nulla voluptate repudiandae quod aperiam veritatis voluptatibus maiores nostrum id nam est. Quibusdam provident animi numquam.',
